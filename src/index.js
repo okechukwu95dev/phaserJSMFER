2f47b51ec554092273e62c3058ddd9c12ec59d79
JOE <okechukwu95@hotmail.com>
2022-03-12 07:40:26 -0600

    added basic game scene

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
+import shipImg from './assets/ship.png';
+import playerSprite from './assets/player.png';
+import {PLAYER_SPRITE_HEIGHT, PLAYER_SPRITE_WIDTH, PLAYER_START_X, PLAYER_START_Y} from './constants'
 
-class MyGame extends Phaser.Scene
+const player = {};
+class MferClubGame extends Phaser.Scene
 {
     constructor ()
     {
@@ -10,30 +13,48 @@ class MyGame extends Phaser.Scene
 
     preload ()
     {
-        this.load.image('logo', logoImg);
+        this.load.image('ship', shipImg);
+        this.load.spritesheet('player', playerSprite,  {
+            frameWidth: PLAYER_SPRITE_WIDTH,
+            frameHeight: PLAYER_SPRITE_HEIGHT 
+
+        });
+
     }
       
     create ()
     {
-        const logo = this.add.image(400, 150, 'logo');
+        // const player = this.add.image(400, 150, 'logo');
+        
+        const ship = this.add.image(0, 0, 'ship');
+        player.sprite = this.add.sprite(PLAYER_START_X, PLAYER_START_Y, 'player');
+
+
       
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
+        // this.tweens.add({
+        //     targets: logo,
+        //     y: 450,
+        //     duration: 2000,
+        //     ease: "Power2",
+        //     yoyo: true,
+        //     loop: -1
+        // });
+    }
+
+    update(){
+        console.log(this.scene);
+        // this.scene.scene.cameras.main.centreOn(player.sprite.x,player.sprite.y);
+        this.scene.scene.cameras.main.centerOn(player.sprite.x,player.sprite.y);
+
     }
 }
 
 const config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: MyGame
+    width: 2000,
+    height: 700,
+    scene: MferClubGame
 };
 
 const game = new Phaser.Game(config);
